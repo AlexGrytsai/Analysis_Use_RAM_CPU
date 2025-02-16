ee42b742b8f991b20d0fcb745f58bfbe4fceabb5
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-16 17:50:03 +0200

    feat: Add analysis scenario for generating IDs in different collections
    This commit introduces a new analysis scenario that evaluates the performance of generating IDs within various collection types (list, deque, set, and dict). It leverages existing ID generation functions and performance monitoring tools to measure execution time and memory usage. This allows for a comparison of the efficiency of different data structures for ID storage and retrieval.

---
+++ analysis_scenarios/list_with_ids.py
@@ -0,0 +1,25 @@
+from collections import deque
+from typing import Tuple
+
+from get_ids import (
+    generate_ids_in_list,
+    generate_ids_in_set,
+    generate_ids_in_deque,
+    generate_ids_in_dict,
+)
+from performance_monitoring.memory import memory_object_report
+from performance_monitoring.timer import timer_of_execution
+
+
+def analyze_performance_generation_list_with_ids(
+    num_ids: int,
+) -> None:
+    ids_list = timer_of_execution(generate_ids_in_list)(num_ids)
+    ids_deque = timer_of_execution(generate_ids_in_deque)(num_ids)
+    ids_set = timer_of_execution(generate_ids_in_set)(num_ids)
+    ids_dict = timer_of_execution(generate_ids_in_dict)(num_ids)
+
+    memory_object_report(ids_list)
+    memory_object_report(ids_deque)
+    memory_object_report(ids_set)
+    memory_object_report(ids_dict)
