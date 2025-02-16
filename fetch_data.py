95f554aa626591f60a59a323beed617cef36bbe6
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-16 13:23:22 +0200

    feat: Add function to fetch test data into a deque
    This commit introduces a new function, `fetch_test_data_in_deque`, which reads test data from a JSON file and stores it in a `deque` object.  This provides an alternative data structure for accessing the test data, potentially improving performance in scenarios that benefit from efficient append and pop operations at both ends of the sequence.

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,5 @@
-from typing import List, Any
+from collections import deque
+from typing import List, Any, Dict
 
 from performance_monitoring.timer import timer_of_execution
 from utils.get_test_data import get_test_data_from_json
@@ -13,7 +14,7 @@ def fetch_test_data_in_list(file_path: str) -> List[dict[str, Any]]:
 
 
 @timer_of_execution
-def fetch_test_data_in_set(file_path: str) -> set[dict[str, Any]] | None:
+def fetch_test_data_in_set(file_path: str) -> set[Dict[str, Any]] | None:
     data_set = set()
     for data in get_test_data_from_json(file_path):
         try:
@@ -22,3 +23,11 @@ def fetch_test_data_in_set(file_path: str) -> set[dict[str, Any]] | None:
             print(f"⛔ Set does not support {type(data)}: {exc}")
             break
     return data_set if data_set else None
+
+
+@timer_of_execution
+def fetch_test_data_in_deque(file_path: str) -> deque[Dict[str, Any]]:
+    data_deque = deque([])
+    for data in get_test_data_from_json(file_path):
+        data_deque.append(data)
+    return data_deque
