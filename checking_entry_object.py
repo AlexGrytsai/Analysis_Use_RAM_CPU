f652c5283fa02f0194e861a37080c7c375edaff2
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-16 09:05:38 +0200

    feat: Add function to check entry object in list
    This commit introduces a new function, `check_entry_object_in_list`, which checks if a given entry object exists within a list of objects. The function utilizes a UUID for default entry objects and includes a timer decorator for performance monitoring.

---
+++ checking_entry_object.py
@@ -0,0 +1,11 @@
+import uuid
+
+from performance_monitoring.timer import timer_of_execution
+
+
+@timer_of_execution
+def check_entry_object_in_list(
+    list_of_objects: list,
+    entry_object: str = uuid.uuid4().hex,
+) -> bool:
+    return entry_object in list_of_objects
