64a61cab706bb41a15556bbbfb3fdc63f5b3f639
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-17 10:14:44 +0200

    perf: Add CPU and RAM monitoring to id generation functions
    This commit adds CPU and RAM monitoring to the `generate_ids_in_list`, `generate_ids_in_deque`, `generate_ids_in_set`, and `generate_ids_in_dict` functions. This will allow us to track the performance of these functions more easily. The decorators `ram_monitor_decorator` and `cpu_monitor_decorator` are used to monitor the RAM and CPU usage, respectively.

--- analysis_scenarios/list_with_ids.py
+++ analysis_scenarios/list_with_ids.py
@@ -4,20 +4,30 @@ from get_ids import (
     generate_ids_in_deque,
     generate_ids_in_dict,
 )
+from performance_monitoring.cpu import cpu_monitor_decorator
 from performance_monitoring.memory import memory_object_report
+from performance_monitoring.ram import ram_monitor_decorator
 
 
 def analyze_performance_generation_list_with_ids(
     num_ids: int,
 ) -> None:
-    ids_list = generate_ids_in_list(num_ids)
+    ids_list = ram_monitor_decorator(is_detail=True)(
+        cpu_monitor_decorator(generate_ids_in_list)
+    )(num_ids)
     memory_object_report(ids_list)
 
-    ids_deque = generate_ids_in_deque(num_ids)
+    ids_deque = ram_monitor_decorator(is_detail=True)(
+        cpu_monitor_decorator(generate_ids_in_deque)
+    )(num_ids)
     memory_object_report(ids_deque)
 
-    ids_set = generate_ids_in_set(num_ids)
+    ids_set = ram_monitor_decorator(is_detail=True)(
+        cpu_monitor_decorator(generate_ids_in_set)
+    )(num_ids)
     memory_object_report(ids_set)
 
-    ids_dict = generate_ids_in_dict(num_ids)
+    ids_dict = ram_monitor_decorator(is_detail=True)(
+        cpu_monitor_decorator(generate_ids_in_dict)
+    )(num_ids)
     memory_object_report(ids_dict)
