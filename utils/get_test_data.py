17b4ac8c3cd884b5d5229699077ffa4543937973
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-17 08:56:42 +0200

    feat: Allow generating multiple copies of test data
    This change introduces a new `num_of_objects` parameter to the `get_test_data_from_json` function, allowing the user to specify how many copies of the data objects from the JSON file should be yielded.
    
    This is useful when needing to generate a larger dataset from a smaller JSON test data file. It uses deepcopy to avoid modifying the original data.

--- utils/get_test_data.py
+++ utils/get_test_data.py
@@ -1,9 +1,17 @@
 import json
+from copy import deepcopy
 from typing import Any, Generator
 
 
-def get_test_data_from_json(file_path: str) -> Generator[dict, Any, None]:
+def get_test_data_from_json(
+    file_path: str, num_of_objects: int
+) -> Generator[dict, Any, None]:
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
-        for obj in data:
-            yield obj
+        counter = 0
+        while counter < num_of_objects:
+            for obj in data:
+                counter += 1
+                if counter > num_of_objects:
+                    break
+                yield deepcopy(obj)
