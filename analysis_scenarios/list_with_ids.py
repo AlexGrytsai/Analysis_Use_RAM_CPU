96cf844f723d40c092e9f8c86da0dd7c9deca898
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-17 09:46:30 +0200

    chore: Remove unused timer import
    The `timer_of_execution` import from `performance_monitoring.timer` was not being used in the `analyze_performance_generation_list_with_ids` function or anywhere else in the file. Removing it cleans up the code and reduces unnecessary dependencies.

--- analysis_scenarios/list_with_ids.py
+++ analysis_scenarios/list_with_ids.py
@@ -5,7 +5,6 @@ from get_ids import (
     generate_ids_in_dict,
 )
 from performance_monitoring.memory import memory_object_report
-from performance_monitoring.timer import timer_of_execution
 
 
 def analyze_performance_generation_list_with_ids(
