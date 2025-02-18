e7be476f88fa4da0864d5782b26e2a1ddb872b49
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-18 08:36:57 +0200

    feat: Enhance CPU usage plotting and add averaging for combined graphs
    This commit introduces several improvements to the CPU usage plotting functions:
    
    - Adds type hinting for improved code clarity and maintainability.
    - Calculates average and maximum CPU usage within `plot_individual_graph_for_cpu` function using the CPU usage data.
    - Modifies `plot_combined_graph_for_cpu` to handle multiple runs of each function by averaging their CPU usage over time. This provides a more robust and representative visualization.
    - Uses `numpy` for averaging CPU usage data.
    - Adds a legend location in combined graph.

--- utils/graphs.py
+++ utils/graphs.py
@@ -1,13 +1,17 @@
+from typing import Dict, List
+
+import numpy as np
 from matplotlib import pyplot as plt
 
 
 def plot_individual_graph_for_cpu(
-    cpu_usage: list,
+    cpu_usage: List[float],
     func_name: str,
     exec_time: float,
-    avg_cpu: float,
-    max_cpu: float,
 ) -> None:
+    avg_cpu = sum(cpu_usage) / len(cpu_usage) if cpu_usage else 0
+    max_cpu = max(cpu_usage, default=0)
+
     plt.figure(figsize=(10, 6))
     time_points = [i * 0.1 for i in range(len(cpu_usage))]
 
@@ -42,15 +46,31 @@ def plot_individual_graph_for_cpu(
     plt.show()
 
 
-def plot_combined_graph_for_cpu(cpu_data: dict) -> None:
+def plot_combined_graph_for_cpu(
+    cpu_data: Dict[str, List[List[float]]],
+) -> None:
     plt.figure(figsize=(10, 6))
 
-    for func_name, data in cpu_data.items():
-        exec_time, avg_cpu, max_cpu, cpu_usage = data
+    for func_name, runs in cpu_data.items():
+        if not runs:
+            continue
+
+        max_length = max(len(run) for run in runs)
+        avg_cpu_usage = np.zeros(max_length)
+
+        for run in runs:
+            avg_cpu_usage[: len(run)] += run
+        avg_cpu_usage /= len(runs)
+
+        time_axis = [i * 0.1 for i in range(max_length)]
+
+        exec_time = max_length * 0.1
+        avg_cpu = np.mean(avg_cpu_usage)
+        max_cpu = np.max(avg_cpu_usage)
 
         plt.plot(
-            [i * 0.1 for i in range(len(cpu_usage))],
-            cpu_usage,
+            time_axis,
+            avg_cpu_usage,
             marker="o",
             label=f"{func_name} (Exec Time: {exec_time:.2f}s, Avg CPU: {avg_cpu:.2f}%, Peak CPU: {max_cpu:.2f}%)",
         )
@@ -77,7 +97,7 @@ def plot_individual_ram_graph(mem_usage, interval, func_name):
     plt.xlabel("Time (seconds)")
     plt.ylabel("RAM Usage (MB)")
     plt.grid(True)
-    plt.legend()
+    plt.legend(loc="upper left")
     plt.show()
 
 
