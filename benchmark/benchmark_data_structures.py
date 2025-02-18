ccf614dca8a08452a29ea642d7496a440ef6dc6d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-18 17:39:46 +0200

    feat: Add set data structure benchmark
    This commit adds support for benchmarking the set data structure when fetching data from Redis. It includes:
    
    - Updating the `fetch_data_from_redis_for_structure` function to   handle set data structures.
    - Adding a `load_set_from_redis` function that uses the above function, with decorators for CPU and memory monitoring.
    - Integrating `load_set_from_redis` into the `benchmark_data_structures` function.

--- benchmark/benchmark_data_structures.py
+++ benchmark/benchmark_data_structures.py
@@ -1,7 +1,7 @@
 import uuid
 from collections import deque
 from time import sleep
-from typing import List, Any, Dict, Union, Type
+from typing import List, Any, Dict, Union, Type, Set
 
 from redis import Redis
 
@@ -13,13 +13,18 @@ from performance_monitoring.ram import ram_monitor_decorator
 def fetch_data_from_redis_for_structure(
     key_list: List[str],
     redis_client: Redis,
-    data_structure: Type[Union[list, deque, dict]],
-) -> Union[List[dict[str, Any]], deque[Dict[str, Any]], Dict[str, Any]]:
+    data_structure: Type[Union[list, deque, dict, set]],
+) -> Union[
+    List[dict[str, Any]], deque[Dict[str, Any]], Dict[str, Any], Set[bytes]
+]:
     if data_structure is list:
         return [redis_client.get(key).decode() for key in key_list]
 
     elif data_structure is deque:
         return deque(redis_client.get(key).decode() for key in key_list)
+
+    elif data_structure is set:
+        return {redis_client.get(key) for key in key_list}
 
     elif data_structure is dict:
         return {
@@ -57,6 +62,15 @@ def load_dict_from_redis(
     return fetch_data_from_redis_for_structure(key_list, redis_client, dict)
 
 
+@ram_monitor_decorator()
+@cpu_monitor_decorator()
+def load_set_from_redis(
+    key_list: List[str],
+    redis_client: Redis,
+) -> Set[bytes]:
+    return fetch_data_from_redis_for_structure(key_list, redis_client, set)
+
+
 def benchmark_data_structures(
     redis_keys: List[str],
     redis_client: Redis,
@@ -66,6 +80,7 @@ def benchmark_data_structures(
         "List": load_list_from_redis,
         "Deque": load_deque_from_redis,
         "Dict": load_dict_from_redis,
+        "Set": load_set_from_redis,
     }
 
     loaded_data = {}
