325015b20a6a2c2fbaafa62c7062e44e4c879328
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-18 11:04:20 +0200

    perf: Simplify and conditionally report memory usage in benchmarks
    The change removes the nested decorator structure in `measure_resource_usage` to simplify resource measurement. The CPU and RAM monitoring decorators are now applied directly to `measure_resource_usage`.
    
    Additionally, adds a `memory_report` flag to `benchmark_id_generation` to conditionally execute `memory_object_report` based on its value. This avoids unnecessary memory reporting and makes the benchmark more flexible.

--- benchmark/benchmark_create_collections.py
+++ benchmark/benchmark_create_collections.py
@@ -13,50 +13,53 @@ from performance_monitoring.memory import memory_object_report
 from performance_monitoring.ram import ram_monitor_decorator
 
 
+@ram_monitor_decorator()
+@cpu_monitor_decorator()
 def measure_resource_usage(
     generator,
     num_ids: int,
-    graph: bool = False,
     **kwargs,
 ) -> Union[list, dict, set, deque]:
-    return ram_monitor_decorator(plot_graph=graph)(
-        cpu_monitor_decorator()(generator)
-    )(num_ids)
+    return generator(num_ids)
 
 
-def benchmark_id_generation(num_ids: int, graph: bool = False) -> None:
+def benchmark_id_generation(
+    num_ids: int,
+    memory_report: bool = False,
+) -> None:
     ids_list = measure_resource_usage(
         generator=generate_ids_in_list,
         num_ids=num_ids,
-        graph=graph,
         func_name="Generate IDs in List",
     )
-    memory_object_report(ids_list)
+
+    if memory_report:
+        memory_object_report(ids_list)
     sleep(1)
 
     ids_deque = measure_resource_usage(
         generator=generate_ids_in_deque,
         num_ids=num_ids,
-        graph=graph,
         func_name="Generate IDs in Deque",
     )
-    memory_object_report(ids_deque)
+    if memory_report:
+        memory_object_report(ids_deque)
     sleep(1)
 
     ids_set = measure_resource_usage(
         generator=generate_ids_in_set,
         num_ids=num_ids,
-        graph=graph,
         func_name="Generate IDs in Set",
     )
-    memory_object_report(ids_set)
+    if memory_report:
+        memory_object_report(ids_set)
     sleep(1)
 
     ids_dict = measure_resource_usage(
         generator=generate_ids_in_dict,
         num_ids=num_ids,
-        graph=graph,
         func_name="Generate IDs in Dict",
     )
-    memory_object_report(ids_dict)
+    if memory_report:
+        memory_object_report(ids_dict)
     sleep(1)
