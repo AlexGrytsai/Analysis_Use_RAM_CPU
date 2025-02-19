2bc7e0dea9e0a9e3ba663fe98d176e003d37d1a6
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 08:49:54 +0200

    refactor: Refactor id generation functions to use redis iterator
    The id generation functions now take a redis iterator as input instead of an integer representing the number of ids to generate.
    This allows the functions to generate ids directly from redis data, improving performance and reducing memory usage. The return types of the functions have also been updated to reflect the changes in the data being processed.
    
    No breaking changes.

--- get_ids.py
+++ get_ids.py
@@ -1,32 +1,40 @@
 from collections import deque
-from typing import List, Dict
+from typing import List, Dict, Generator, Any, Iterator
 
 from utils.uuid_generator import generate_uuids
 
 
-def generate_ids_in_list(num_of_ids: int) -> List[str]:
+def generate_ids_in_list(
+    redis_iterator: Iterator[bytes],
+) -> List[str]:
     ids_list = []
-    for uid in generate_uuids(num_of_ids):
-        ids_list.append(uid)
+    for uid in redis_iterator:
+        ids_list.append(uid.decode())
     return ids_list
 
 
-def generate_ids_in_set(num_of_ids: int) -> set[str]:
+def generate_ids_in_set(
+    redis_iterator: Iterator[bytes],
+) -> set[bytes]:
     ids_set = set()
-    for uid in generate_uuids(num_of_ids):
+    for uid in redis_iterator:
         ids_set.add(uid)
     return ids_set
 
 
-def generate_ids_in_deque(num_of_ids: int) -> deque[str]:
+def generate_ids_in_deque(
+    redis_iterator: Iterator[bytes],
+) -> deque[str]:
     ids_deque = deque([])
-    for uid in generate_uuids(num_of_ids):
-        ids_deque.append(uid)
+    for uid in redis_iterator:
+        ids_deque.append(uid.decode())
     return ids_deque
 
 
-def generate_ids_in_dict(num_of_ids: int) -> Dict[str, str]:
+def generate_ids_in_dict(
+    redis_iterator: Iterator[bytes],
+) -> Dict[str, None]:
     ids_dict = {}
-    for uid in generate_uuids(num_of_ids):
-        ids_dict[uid] = uid
+    for uid in redis_iterator:
+        ids_dict[uid.decode()] = None
     return ids_dict
