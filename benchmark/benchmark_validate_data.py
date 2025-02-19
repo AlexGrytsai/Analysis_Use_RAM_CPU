8fee200ccba35a9990746d42fd46870dcc971cc5
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 10:37:52 +0200

    feat: Add benchmark_validate_data function
    This commit introduces the `benchmark_validate_data` function to benchmark the performance of validating data fetched from Redis when stored in different data structures (list, deque, dict, set).
    
    The function fetches data from Redis using specified keys and a
    Redis client. It then validates the data using the
    `TenderDataValidator` and measures the time taken for each data
    structure. An optional `memory_report` argument enables reporting
    memory usage for each structure.

--- benchmark/benchmark_validate_data.py
+++ benchmark/benchmark_validate_data.py
@@ -1,7 +1,15 @@
+import json
 from collections import deque
+from time import sleep
 from typing import List, Any, Dict, Set
 
+from redis import Redis
+
+from benchmark.benchmark_data_structures import (
+    fetch_data_from_redis_for_structure,
+)
 from performance_monitoring.cpu import cpu_monitor_decorator
+from performance_monitoring.memory import memory_object_report
 from performance_monitoring.ram import ram_monitor_decorator
 from utils.validators import TenderDataValidator
 
@@ -49,6 +57,45 @@ def validate_data_from_set(
 ) -> Set[str]:
     clean_data = set()
     for value in raw_data_set:
-        validate_date = TenderDataValidator(**value.decode())
-        clean_data.add(validate_date.model_dump_json())
+        clean_data.add(
+            TenderDataValidator(**json.loads(value)).model_dump_json()
+        )
     return clean_data
+
+
+def benchmark_validate_data(
+    redis_keys: List[str],
+    redis_client: Redis,
+    memory_report: bool = False,
+) -> None:
+    validated_data_from_list = validate_data_from_list(
+        raw_data_list=fetch_data_from_redis_for_structure(
+            redis_keys, redis_client, list
+        )
+    )
+    sleep(1)
+
+    validated_data_from_deque = validate_data_from_deque(
+        raw_data_deque=fetch_data_from_redis_for_structure(
+            redis_keys, redis_client, deque
+        )
+    )
+    sleep(1)
+
+    validated_data_from_dict = validate_data_from_dict(
+        raw_data_dict=fetch_data_from_redis_for_structure(
+            redis_keys, redis_client, dict
+        )
+    )
+    validated_data_from_set = validate_data_from_set(
+        raw_data_set=fetch_data_from_redis_for_structure(
+            redis_keys, redis_client, set
+        )
+    )
+    sleep(1)
+
+    if memory_report:
+        memory_object_report(validated_data_from_list)
+        memory_object_report(validated_data_from_deque)
+        memory_object_report(validated_data_from_dict)
+        memory_object_report(validated_data_from_set)
