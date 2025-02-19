9f0aee556000da5a33c3992c8c8c7a3ca0b6595f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 07:57:11 +0200

    feat: Add functions for Redis data preparation
    This commit introduces new functions to facilitate the preparation of data in Redis for benchmarking purposes.
    
    - `change_object_id_in_test_data`: Modifies the tenderID within a test data object.
    - `generate_uuid_keys_for_redis`: Generates a specified number of UUID keys for Redis.
    - `add_empty_keys_to_redis`: Adds a specified number of empty keys to Redis.
    - `prepare_data_for_benchmark`: Combines adding empty keys and data from a file to Redis, allowing for controlled data population for benchmarking.

--- utils/redis_test_data_loader.py
+++ utils/redis_test_data_loader.py
@@ -5,8 +5,14 @@ from typing import Any, Generator, Tuple
 from redis import Redis
 
 
+def change_object_id_in_test_data(obj: dict, new_id: str) -> dict:
+    obj["data"]["tenderID"] = new_id
+    return obj
+
+
 def get_test_data_from_json(
-    file_path: str, num_of_objects: int
+    file_path: str,
+    num_of_objects: int,
 ) -> Generator[Tuple[Any, str], Any, None]:
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -17,8 +23,14 @@ def get_test_data_from_json(
                 if counter > num_of_objects:
                     break
                 key = uuid.uuid4().hex
-                obj["data"]["tenderID"] = key
-                yield obj, key
+                yield change_object_id_in_test_data(obj, key), key
+
+
+def generate_uuid_keys_for_redis(
+    num_of_keys: int,
+) -> Generator[str, Any, None]:
+    for _ in range(num_of_keys):
+        yield uuid.uuid4().hex
 
 
 def add_data_to_redis(
@@ -29,6 +41,25 @@ def add_data_to_redis(
     for data, key in get_test_data_from_json(
         file_path=file_path, num_of_objects=num_of_objects
     ):
-        redis_client.set(
-            key, json.dumps(data, ensure_ascii=False)
-        )
+        redis_client.set(key, json.dumps(data, ensure_ascii=False))
+
+
+def add_empty_keys_to_redis(redis_client: Redis, num_of_keys: int) -> None:
+    for key in generate_uuid_keys_for_redis(num_of_keys=num_of_keys):
+        redis_client.set(key, "")
+
+
+def prepare_data_for_benchmark(
+    redis_client: Redis,
+    total_keys: int,
+    num_keys_with_data: int,
+    file_path: str,
+) -> None:
+    add_empty_keys_to_redis(
+        redis_client=redis_client, num_of_keys=total_keys - num_keys_with_data
+    )
+    add_data_to_redis(
+        redis_client=redis_client,
+        num_of_objects=num_keys_with_data,
+        file_path=file_path,
+    )
