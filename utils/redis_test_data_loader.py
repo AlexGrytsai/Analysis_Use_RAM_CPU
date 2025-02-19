2158e4efbaa6df4f8a6adf91f0a6902fb0e6a847
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 11:55:07 +0200

    feat: Add redis test data loader utilities
    This commit introduces utilities for loading and managing test data in Redis for benchmarking and testing purposes.
    
    - Adds functions for generating UUID keys, retrieving test data from JSON files, and populating Redis with both data-filled and empty keys.
    - Introduces `create_list_iterator_from_redis_keys` to retrieve multiple redis keys iterators
    - Includes a `clean_redis_database` function to flush the Redis database before or after tests.
    - Refactors `add_data_to_redis` and `add_empty_keys_to_redis` to accept redis client.
    - Implements `prepare_data_for_benchmark` to set up a mixed dataset of data-filled and empty keys for realistic benchmarking scenarios.

--- utils/redis_test_data_loader.py
+++ utils/redis_test_data_loader.py
@@ -1,13 +1,23 @@
 import json
 import uuid
-from typing import Any, Generator, Tuple, List
+from typing import Any, Generator, Tuple, List, Iterator
 
+import redis
 from redis import Redis
+
+redis_client = redis.Redis(host="localhost", port=6379, db=3)
 
 
 def change_object_id_in_test_data(obj: dict, new_id: str) -> dict:
     obj["data"]["tenderID"] = new_id
     return obj
+
+
+def create_list_iterator_from_redis_keys(
+    r_client: Redis = redis_client,
+    num_iter: int = 4,
+) -> List[Iterator[bytes]]:
+    return [r_client.scan_iter("*") for _ in range(num_iter)]
 
 
 def get_test_data_from_json(
@@ -33,8 +43,12 @@ def generate_uuid_keys_for_redis(
         yield uuid.uuid4().hex
 
 
+def clean_redis_database(r_client: Redis = redis_client) -> None:
+    r_client.flushdb()
+
+
 def add_data_to_redis(
-    redis_client: Redis,
+    r_client: Redis,
     num_of_objects: int,
     file_path: str,
 ) -> List[str]:
@@ -42,27 +56,27 @@ def add_data_to_redis(
     for data, key in get_test_data_from_json(
         file_path=file_path, num_of_objects=num_of_objects
     ):
-        redis_client.set(key, json.dumps(data, ensure_ascii=False))
+        r_client.set(key, json.dumps(data, ensure_ascii=False))
         list_key_with_data.append(key)
     return list_key_with_data
 
 
-def add_empty_keys_to_redis(redis_client: Redis, num_of_keys: int) -> None:
+def add_empty_keys_to_redis(r_client: Redis, num_of_keys: int) -> None:
     for key in generate_uuid_keys_for_redis(num_of_keys=num_of_keys):
-        redis_client.set(key, "")
+        r_client.set(key, "")
 
 
 def prepare_data_for_benchmark(
-    redis_client: Redis,
     total_keys: int,
     num_keys_with_data: int,
     file_path: str,
+    r_client: Redis = redis_client,
 ) -> List[str]:
     add_empty_keys_to_redis(
-        redis_client=redis_client, num_of_keys=total_keys - num_keys_with_data
+        r_client=r_client, num_of_keys=total_keys - num_keys_with_data
     )
     return add_data_to_redis(
-        redis_client=redis_client,
+        r_client=r_client,
         num_of_objects=num_keys_with_data,
         file_path=file_path,
     )
