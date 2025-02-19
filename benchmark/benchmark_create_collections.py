8e98e20e5a93fe204dbea413c4da5c7a7a8f6b65
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 08:50:51 +0200

    refactor: Refactor benchmark_id_generation function
    This commit refactors the benchmark_id_generation function to accept a list of iterators instead of a single number of IDs. This change allows for more flexibility in the benchmarking process, as different iterators can be used to generate IDs. The measure_resource_usage function now accepts a redis_iterator and a callable function.
    The memory_object_report calls were moved to the end of the function.

--- benchmark/benchmark_create_collections.py
+++ benchmark/benchmark_create_collections.py
@@ -1,6 +1,6 @@
 from collections import deque
 from time import sleep
-from typing import Union
+from typing import Union, Callable, Iterator, List
 
 from get_ids import (
     generate_ids_in_list,
@@ -16,50 +16,49 @@ from performance_monitoring.ram import ram_monitor_decorator
 @ram_monitor_decorator()
 @cpu_monitor_decorator()
 def measure_resource_usage(
-    generator,
-    num_ids: int,
+    redis_iterator: Iterator[bytes],
+    func: Callable,
     **kwargs,
 ) -> Union[list, dict, set, deque]:
-    return generator(num_ids)
+    return func(redis_iterator)
 
 
 def benchmark_id_generation(
-    num_ids: int,
+    list_iterator: List[Iterator[bytes]],
     memory_report: bool = False,
 ) -> None:
+    iterator_1, iterator_2, iterator_3, iterator_4 = list_iterator
+
     ids_list = measure_resource_usage(
-        generator=generate_ids_in_list,
-        num_ids=num_ids,
+        func=generate_ids_in_list,
+        redis_iterator=iterator_1,
         func_name="Generate IDs in List",
     )
-
-    if memory_report:
-        memory_object_report(ids_list)
     sleep(1)
 
     ids_deque = measure_resource_usage(
-        generator=generate_ids_in_deque,
-        num_ids=num_ids,
+        func=generate_ids_in_deque,
+        redis_iterator=iterator_2,
         func_name="Generate IDs in Deque",
     )
-    if memory_report:
-        memory_object_report(ids_deque)
     sleep(1)
 
     ids_set = measure_resource_usage(
-        generator=generate_ids_in_set,
-        num_ids=num_ids,
+        func=generate_ids_in_set,
+        redis_iterator=iterator_3,
         func_name="Generate IDs in Set",
     )
-    if memory_report:
-        memory_object_report(ids_set)
     sleep(1)
 
     ids_dict = measure_resource_usage(
-        generator=generate_ids_in_dict,
-        num_ids=num_ids,
+        func=generate_ids_in_dict,
+        redis_iterator=iterator_4,
         func_name="Generate IDs in Dict",
     )
-    if memory_report:
-        memory_object_report(ids_dict)
     sleep(1)
+
+    if memory_report:
+        memory_object_report(ids_list)
+        memory_object_report(ids_deque)
+        memory_object_report(ids_set)
+        memory_object_report(ids_dict)
