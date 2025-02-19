79796b5be512463a6ca05bcdead27ee4052e5d5f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-19 12:06:58 +0200

    refactor: Decouple data loading from Redis client
    This commit refactors the benchmark data structures code to remove the direct dependency on the Redis client within the data loading functions. This improves modularity and testability.
    
    - The `fetch_data_from_redis_for_structure` function is now solely responsible for fetching data based on the provided `data_structure` argument.
    - The data loading functions (e.g., `load_list_from_redis`, `load_deque_from_redis`) now only receive the `key_list` argument and use the `fetch_data_from_redis_for_structure` function internally.
    - The `benchmark_data_structures` function no longer requires the `redis_client` argument, simplifying its interface.
    - Added main function to run the benchmarks, show memory reports, and generate graphs for CPU and RAM usage.
    - Added graphs utils to plot combined graphs for CPU and RAM usage.

--- benchmark/benchmark_data_structures.py
+++ benchmark/benchmark_data_structures.py
@@ -1,87 +1,60 @@
-import json
-import uuid
 from collections import deque
 from time import sleep
-from typing import List, Any, Dict, Union, Type, Set
+from typing import List, Any, Dict, Set
 
-from redis import Redis
-
-from performance_monitoring.cpu import cpu_monitor_decorator
+from performance_monitoring.cpu import cpu_monitor_decorator, cpu_usage_results
 from performance_monitoring.memory import memory_object_report
-from performance_monitoring.ram import ram_monitor_decorator
-
-
-def fetch_data_from_redis_for_structure(
-    key_list: List[str],
-    redis_client: Redis,
-    data_structure: Type[Union[list, deque, dict, set]],
-    **kwargs,
-) -> Union[
-    List[dict[str, Any]], deque[Dict[str, Any]], Dict[str, Any], Set[bytes]
-]:
-    if data_structure is list:
-        return [json.loads(redis_client.get(key).decode()) for key in key_list]
-
-    elif data_structure is deque:
-        return deque(
-            json.loads(redis_client.get(key).decode()) for key in key_list
-        )
-
-    elif data_structure is set:
-        return {redis_client.get(key) for key in key_list}
-
-    elif data_structure is dict:
-        return {
-            uuid.uuid4().hex: json.loads(redis_client.get(key).decode())
-            for key in key_list
-        }
-
-    raise ValueError(f"Unsupported data structure: {data_structure}")
+from performance_monitoring.ram import ram_monitor_decorator, ram_usage_results
+from utils.graphs import plot_combined_graph_for_cpu, plot_combined_ram_graph
+from utils.redis_test_data_loader import fetch_data_from_redis_for_structure
 
 
 @ram_monitor_decorator()
 @cpu_monitor_decorator()
 def load_list_from_redis(
     key_list: List[str],
-    redis_client: Redis,
     **kwargs,
 ) -> List[dict[str, Any]]:
-    return fetch_data_from_redis_for_structure(key_list, redis_client, list)
+    return fetch_data_from_redis_for_structure(
+        key_list=key_list, data_structure=list
+    )
 
 
 @ram_monitor_decorator()
 @cpu_monitor_decorator()
 def load_deque_from_redis(
     key_list: List[str],
-    redis_client: Redis,
     **kwargs,
 ) -> deque[Dict[str, Any]]:
-    return fetch_data_from_redis_for_structure(key_list, redis_client, deque)
+    return fetch_data_from_redis_for_structure(
+        key_list=key_list, data_structure=deque
+    )
 
 
 @ram_monitor_decorator()
 @cpu_monitor_decorator()
 def load_dict_from_redis(
     key_list: List[str],
-    redis_client: Redis,
     **kwargs,
 ) -> Dict[str, Any]:
-    return fetch_data_from_redis_for_structure(key_list, redis_client, dict)
+    return fetch_data_from_redis_for_structure(
+        key_list=key_list, data_structure=dict
+    )
 
 
 @ram_monitor_decorator()
 @cpu_monitor_decorator()
 def load_set_from_redis(
     key_list: List[str],
-    redis_client: Redis,
     **kwargs,
 ) -> Set[bytes]:
-    return fetch_data_from_redis_for_structure(key_list, redis_client, set)
+    return fetch_data_from_redis_for_structure(
+        key_list=key_list, data_structure=set
+    )
 
 
 def benchmark_data_structures(
     redis_keys: List[str],
-    redis_client: Redis,
     memory_report: bool = False,
 ) -> None:
     data_structures = {
@@ -95,7 +68,6 @@ def benchmark_data_structures(
     for name, func in data_structures.items():
         loaded_data[name] = func(
             key_list=redis_keys,
-            redis_client=redis_client,
             func_name=f"Load data to {name}",
         )
         sleep(1)
@@ -103,3 +75,17 @@ def benchmark_data_structures(
     if memory_report:
         for _, data in loaded_data.items():
             memory_object_report(data)
+
+
+def main_benchmark_add_data(
+    list_keys: List[str],
+    repeat: int = 10,
+) -> None:
+    for _ in range(repeat):
+        benchmark_data_structures(
+            redis_keys=list_keys,
+            memory_report=True,
+        )
+
+    plot_combined_graph_for_cpu(cpu_data=cpu_usage_results)
+    plot_combined_ram_graph(ram_data=ram_usage_results)
