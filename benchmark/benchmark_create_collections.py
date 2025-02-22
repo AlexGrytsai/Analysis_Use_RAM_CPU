12cd288f65d15d7269b3fad9c2a65514999ebfbb
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-22 08:23:50 +0200

    perf: Refactor benchmark_create_collections for better performance
    This commit refactors the `benchmark_create_collections.py` script to improve its structure and performance measurement capabilities.
    Specifically, it introduces a more flexible approach to benchmarking different collection types by passing the collection creation function as an argument to the main benchmarking function, which makes code more modular and reusable. Additionally, the redis iterator is passed as a parameter to the function instead of creating multiple iterators.

--- benchmark/benchmark_create_collections.py
+++ benchmark/benchmark_create_collections.py
@@ -1,18 +1,18 @@
 from collections import deque
-from time import sleep
-from typing import Union, Callable, Iterator, List
+from typing import Union, Callable, Iterator, Optional
 
-from performance_monitoring.cpu import cpu_monitor_decorator, cpu_usage_results
+from performance_monitoring.cpu import cpu_monitor_decorator
 from performance_monitoring.memory import memory_object_report
-from performance_monitoring.ram import ram_monitor_decorator, ram_usage_results
+from performance_monitoring.ram import ram_monitor_decorator
 from utils.get_ids import (
     generate_ids_in_list,
-    generate_ids_in_set,
     generate_ids_in_deque,
+    generate_ids_in_set,
     generate_ids_in_dict,
 )
-from utils.graphs import plot_combined_graph_for_cpu, plot_combined_ram_graph
-from utils.redis_test_data_loader import create_list_iterator_from_redis_keys
+from utils.redis_test_data_loader import (
+    redis_client,
+)
 
 
 @ram_monitor_decorator()
@@ -26,48 +26,55 @@ def measure_resource_usage(
 
 
 def benchmark_id_generation(
-    list_iterator: List[Iterator[bytes]],
-    memory_report: bool = False,
+    func: Callable,
+    func_name: str,
+    iterator: Iterator[bytes],
+    memory_report: Optional[bool] = False,
 ) -> None:
-    iterator_1, iterator_2, iterator_3, iterator_4 = list_iterator
-
-    ids_list = measure_resource_usage(
-        func=generate_ids_in_list,
-        redis_iterator=iterator_1,
-        func_name="Generate IDs in List",
-    )
-
-    ids_deque = measure_resource_usage(
-        func=generate_ids_in_deque,
-        redis_iterator=iterator_2,
-        func_name="Generate IDs in Deque",
-    )
-
-    ids_set = measure_resource_usage(
-        func=generate_ids_in_set,
-        redis_iterator=iterator_3,
-        func_name="Generate IDs in Set",
-    )
-
-    ids_dict = measure_resource_usage(
-        func=generate_ids_in_dict,
-        redis_iterator=iterator_4,
-        func_name="Generate IDs in Dict",
+    ids_collection = measure_resource_usage(
+        func=func,
+        redis_iterator=iterator,
+        func_name=func_name,
     )
 
     if memory_report:
-        memory_object_report(ids_list)
-        memory_object_report(ids_deque)
-        memory_object_report(ids_set)
-        memory_object_report(ids_dict)
+        memory_object_report(ids_collection)
 
 
-def main_benchmark_ids_generation(repeat: int = 1) -> None:
+def main_benchmark_ids_generation(
+    func: Callable, func_name: str, repeat: int = 10
+) -> None:
+    """
+    func:
+        - generate_ids_in_list
+        - generate_ids_in_deque
+        - generate_ids_in_set
+        - generate_ids_in_dict
+    func_name:
+        - Generate IDs in List
+        - Generate IDs in Deque
+        - Generate IDs in Set
+        - Generate IDs in Dict
+    """
     for _ in range(repeat):
         benchmark_id_generation(
-            list_iterator=create_list_iterator_from_redis_keys(),
+            func=func,
+            func_name=func_name,
+            iterator=redis_client.scan_iter("*"),
             memory_report=False,
         )
 
-    plot_combined_graph_for_cpu(cpu_data=cpu_usage_results)
-    plot_combined_ram_graph(ram_data=ram_usage_results)
+
+if __name__ == "__main__":
+    main_benchmark_ids_generation(
+        func=generate_ids_in_list, func_name="Generate IDs in List"
+    )
+    main_benchmark_ids_generation(
+        func=generate_ids_in_deque, func_name="Generate IDs in Deque"
+    )
+    main_benchmark_ids_generation(
+        func=generate_ids_in_set, func_name="Generate IDs in Set"
+    )
+    main_benchmark_ids_generation(
+        func=generate_ids_in_dict, func_name="Generate IDs in Dict"
+    )
