0779b35b3c551d6f667e9474b2421555797268e9
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 11:30:50 +0200

    refactor: Refactor data validation benchmark
    This commit refactors the data validation benchmark to:
    
    - Generalize the `benchmark_validate_data` function to accept different data structures (list, deque, dict, set) directly.
    - Remove individual validation functions for each data structure and replace them with a single, unified `validate` function.
    - Add a `run_benchmark_validate_data` function to run the benchmark for different data structure types.
    - Improve code readability and maintainability.
    - Add size checking functionality.

--- benchmark/benchmark_validate_data.py
+++ benchmark/benchmark_validate_data.py
@@ -1,103 +1,77 @@
 import json
 from collections import deque
-from time import sleep
-from typing import List, Any, Dict, Set
+from typing import List, Any, Dict, Set, Union, Type
 
-from benchmark.benchmark_data_structures import (
-    fetch_data_from_redis_for_structure,
-)
-from performance_monitoring.cpu import cpu_monitor_decorator, cpu_usage_results
-from performance_monitoring.memory import memory_object_report
-from performance_monitoring.ram import ram_monitor_decorator, ram_usage_results
-from utils.graphs import plot_combined_graph_for_cpu, plot_combined_ram_graph
+from pympler import asizeof
+
+from performance_monitoring.cpu import cpu_monitor_decorator
+from performance_monitoring.ram import ram_monitor_decorator
 from utils.redis_for_save_usage_data import usage_r_client
+from utils.redis_test_data_loader import fetch_data_from_redis_for_structure
 from utils.validators import TenderDataValidator
 
-
-@cpu_monitor_decorator(r_client=usage_r_client)
-@ram_monitor_decorator(r_client=usage_r_client)
-def validate_data_from_list(
-    raw_data_list: List[Dict[str, Any]],
-) -> List[TenderDataValidator]:
-    clean_data = []
-    for data in raw_data_list:
-        validate_date = TenderDataValidator(**data)
-        clean_data.append(validate_date)
-    return clean_data
+CLEANED_DATA_TYPE = (
+    Union[
+        List[TenderDataValidator],
+        deque[TenderDataValidator],
+        Dict[str, TenderDataValidator],
+        Set[str],
+    ],
+)
 
 
-@cpu_monitor_decorator(r_client=usage_r_client)
-@ram_monitor_decorator(r_client=usage_r_client)
-def validate_data_from_deque(
-    raw_data_deque: deque,
-) -> deque[TenderDataValidator]:
-    clean_data = deque([])
-    for data in raw_data_deque:
-        validate_date = TenderDataValidator(**data)
-        clean_data.append(validate_date)
-    return clean_data
+def validate(item: Dict[str, Any]) -> TenderDataValidator:
+    return TenderDataValidator(**item)
 
 
 @cpu_monitor_decorator(r_client=usage_r_client)
 @ram_monitor_decorator(r_client=usage_r_client)
-def validate_data_from_dict(
-    raw_data_dict: Dict[str, Any],
-) -> Dict[str, TenderDataValidator]:
-    clean_data = {}
-    for key, value in raw_data_dict.items():
-        validate_date = TenderDataValidator(**value)
-        clean_data[key] = validate_date
-    return clean_data
-
-
-@cpu_monitor_decorator(r_client=usage_r_client)
-@ram_monitor_decorator(r_client=usage_r_client)
-def validate_data_from_set(
-    raw_data_set: Set,
-) -> Set[str]:
-    clean_data = set()
-    for value in raw_data_set:
-        clean_data.add(
-            TenderDataValidator(**json.loads(value)).model_dump_json()
-        )
-    return clean_data
-
-
 def benchmark_validate_data(
-    redis_keys: List[str],
-    memory_report: bool = False,
+    raw_data: Union[List, deque, Dict, Set[str]],
+    **kwargs,
+) -> CLEANED_DATA_TYPE:
+    if isinstance(raw_data, list):
+        return [validate(data) for data in raw_data]
+
+    elif isinstance(raw_data, deque):
+        return deque(validate(data) for data in raw_data)
+
+    elif isinstance(raw_data, dict):
+        return {key: validate(value) for key, value in raw_data.items()}
+
+    elif isinstance(raw_data, set):
+        return {
+            validate(json.loads(value)).model_dump_json() for value in raw_data
+        }
+
+    else:
+        raise ValueError(f"Unsupported data structure: {type(raw_data)}")
+
+
+def run_benchmark_validate_data(
+    type_collection: Type[Union[list, deque, dict, set]],
+    is_print_size: bool = False,
 ) -> None:
-    validated_data_from_list = validate_data_from_list(
-        raw_data_list=fetch_data_from_redis_for_structure(
-            key_list=redis_keys, data_structure=list
-        )
+    data_for_benchmark = {
+        list: "Create List with clean data",
+        deque: "Create Deque with clean data",
+        set: "Create Set with clean data",
+        dict: "Create Dict with clean data",
+    }
+
+    clean_data = benchmark_validate_data(
+        raw_data=fetch_data_from_redis_for_structure(
+            data_structure=type_collection
+        ),
+        func_name=data_for_benchmark[type_collection],
     )
 
-    validated_data_from_deque = validate_data_from_deque(
-        raw_data_deque=fetch_data_from_redis_for_structure(
-            key_list=redis_keys, data_structure=deque
+    if is_print_size:
+        print(
+            f"Size of {data_for_benchmark[type_collection]}: "
+            f"{asizeof.asizeof(clean_data) / 1024 / 1024:.2f} MB"
         )
-    )
-
-    validated_data_from_dict = validate_data_from_dict(
-        raw_data_dict=fetch_data_from_redis_for_structure(
-            key_list=redis_keys, data_structure=dict
-        )
-    )
-    validated_data_from_set = validate_data_from_set(
-        raw_data_set=fetch_data_from_redis_for_structure(
-            key_list=redis_keys, data_structure=set
-        )
-    )
-
-    if memory_report:
-        memory_object_report(validated_data_from_list)
-        memory_object_report(validated_data_from_deque)
-        memory_object_report(validated_data_from_dict)
-        memory_object_report(validated_data_from_set)
 
 
-def main_benchmark_validate_data(
-    list_keys: List[str],
-) -> None:
-    benchmark_validate_data(redis_keys=list_keys, memory_report=False)
+if __name__ == "__main__":
+    run_benchmark_validate_data(type_collection=set, is_print_size=True)
