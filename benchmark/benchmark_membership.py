f84101184c7abb72bc0a424f371d879e6da257ad
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 07:30:41 +0200

    perf: Optimize membership test and add collection creation
    This commit optimizes the membership test by incorporating CPU and RAM monitoring decorators. It also introduces a function to create collections with simple data, and restructures the main section to directly benchmark membership performance for lists using data retrieved from Redis. This improves performance analysis and simplifies the benchmark execution.

--- benchmark/benchmark_membership.py
+++ benchmark/benchmark_membership.py
@@ -1,22 +1,26 @@
 from collections import deque
-from time import sleep
-from typing import Union, List, Dict, Set, Iterator
+from typing import Union, List, Dict
 
+from benchmark.benchmark_create_collections import (
+    create_collection_with_simple_data,
+)
+from performance_monitoring.cpu import cpu_monitor_decorator
+from performance_monitoring.ram import ram_monitor_decorator
 from utils.get_ids import (
     generate_ids_in_list,
-    generate_ids_in_set,
-    generate_ids_in_deque,
-    generate_ids_in_dict,
 )
-from performance_monitoring.cpu import cpu_monitor_decorator, cpu_usage_results
-from performance_monitoring.ram import ram_monitor_decorator, ram_usage_results
-from utils.graphs import plot_combined_graph_for_cpu, plot_combined_ram_graph
-from utils.redis_test_data_loader import create_list_iterator_from_redis_keys
+from utils.redis_for_save_usage_data import usage_r_client
+from utils.redis_test_data_loader import (
+    r_client_prepare,
+)
 
 
+@cpu_monitor_decorator(r_client=usage_r_client)
+@ram_monitor_decorator(r_client=usage_r_client)
 def benchmark_membership_test(
     collection: Union[List[str], Dict[str, str], set[str], deque[str]],
     list_keys_with_data: List[str],
+    **kwargs,
 ) -> None:
     for obj in list_keys_with_data:
         if obj in collection:
@@ -25,65 +29,23 @@ def benchmark_membership_test(
             continue
 
 
-@ram_monitor_decorator()
-@cpu_monitor_decorator()
-def analyze_membership_performance(
-    collection: Union[List[str], Dict[str, None], Set[str], deque],
-    list_keys_with_data: List[str],
-    **kwargs,
-) -> None:
-    benchmark_membership_test(
-        collection=collection,
-        list_keys_with_data=list_keys_with_data,
+if __name__ == "__main__":
+    """
+    collection:
+        - List IDs -> generate_ids_in_list -> Membership performance for List
+        - Deque IDs-> generate_ids_in_deque -> Membership performance for Deque
+        - Set IDs -> generate_ids_in_set -> Membership performance for Set
+        - Dict IDs -> generate_ids_in_dict -> Membership performance for Dict
+    """
+    list_ids_with_data = r_client_prepare.lrange("list_keys_with_data", 0, -1)
+
+    ids_collection = create_collection_with_simple_data(
+        func=generate_ids_in_list,
+        redis_iterator=r_client_prepare.scan_iter("*"),
     )
 
-
-def run_membership_benchmark(
-    list_iterator: List[Iterator[bytes]], list_keys_with_data: List[str]
-) -> None:
-    iter_1, iter_2, iter_3, iter_4 = list_iterator
-
-    list_ids = generate_ids_in_list(iter_1)
-    deque_ids = generate_ids_in_deque(iter_2)
-    set_ids = generate_ids_in_set(iter_3)
-    dict_ids = generate_ids_in_dict(iter_4)
-    sleep(2)
-
-    analyze_membership_performance(
-        collection=list_ids,
-        list_keys_with_data=list_keys_with_data,
+    benchmark_membership_test(
+        collection=ids_collection,
+        list_keys_with_data=list_ids_with_data,
         func_name="Membership performance for List",
     )
-    sleep(1)
-
-    analyze_membership_performance(
-        collection=deque_ids,
-        list_keys_with_data=list_keys_with_data,
-        func_name="Membership performance for Deque",
-    )
-    sleep(1)
-
-    analyze_membership_performance(
-        collection=set_ids,
-        list_keys_with_data=list_keys_with_data,
-        func_name="Membership performance for Set",
-    )
-    sleep(1)
-
-    analyze_membership_performance(
-        collection=dict_ids,
-        list_keys_with_data=list_keys_with_data,
-        func_name="Membership performance for Dict",
-    )
-    sleep(1)
-
-
-def main_benchmark_membership(list_keys: List[str], repeat: int = 1) -> None:
-    for _ in range(repeat):
-        run_membership_benchmark(
-            list_keys_with_data=list_keys,
-            list_iterator=create_list_iterator_from_redis_keys(),
-        )
-
-    plot_combined_graph_for_cpu(cpu_data=cpu_usage_results)
-    plot_combined_ram_graph(ram_data=ram_usage_results, y_limit=130)
