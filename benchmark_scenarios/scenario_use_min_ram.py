8ce5beccca6974b5743e23b8202b528f6cc60967
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 13:45:05 +0200

    refactor: Rename and add new benchmark scenarios
    This commit renames existing benchmark scenarios to more generic `scenario_v1` and `scenario_v2`. It also introduces new benchmark scenarios: `scenario_v3`, `scenario_v4`, `scenario_v5`, and `scenario_v6`. These scenarios explore different ways of processing data retrieved from Redis, focusing on optimizing performance and memory usage. The addition of new scenarios facilitates a more comprehensive benchmark suite, enabling a deeper understanding of the performance characteristics of different data processing approaches.
    The use of `get_list_keys_with_data` was removed as it is no longer used.
    Each scenario uses `cpu_monitor_decorator` and `ram_monitor_decorator` to track resource usage.
    kwargs was added to scenarios.

--- benchmark_scenarios/scenario_use_min_ram.py
+++ benchmark_scenarios/scenario_use_min_ram.py
@@ -6,15 +6,14 @@ from utils.redis_for_save_usage_data import usage_r_client
 from utils.redis_test_data_loader import (
     create_iterator_from_redis_keys,
     get_value_from_redis,
-    get_list_keys_with_data,
 )
 from utils.validators import TenderDataValidator
 
 
 @cpu_monitor_decorator(r_client=usage_r_client)
 @ram_monitor_decorator(r_client=usage_r_client)
-def scenario_use_min_ram_sequence_not_important_v1(
-    key_with_data: List[str],
+def scenario_v1(
+    key_with_data: List[str], **kwargs
 ) -> list[TenderDataValidator]:
     all_ids_list = []
     redis_iterator = create_iterator_from_redis_keys()
@@ -24,8 +23,8 @@ def scenario_use_min_ram_sequence_not_important_v1(
     all_ids_set = set(all_ids_list)
     membership_list = []
 
-    for id_data in key_with_data:
-        if id_data in all_ids_set:
+    for id_data in all_ids_set:
+        if id_data in set(key_with_data):
             membership_list.append(id_data)
 
     raw_data_list = []
@@ -45,13 +44,13 @@ def scenario_use_min_ram_sequence_not_important_v1(
 
 @cpu_monitor_decorator(r_client=usage_r_client)
 @ram_monitor_decorator(r_client=usage_r_client)
-def scenario_use_min_ram_sequence_not_important_v2(
-    key_with_data: List[str],
+def scenario_v2(
+    key_with_data: List[str], **kwargs
 ) -> list[TenderDataValidator]:
     all_ids_list = [id_ for id_ in create_iterator_from_redis_keys()]
 
     all_ids_set = set(all_ids_list)
-    membership_list = [id_ for id_ in key_with_data if id_ in all_ids_set]
+    membership_list = set(key_with_data) & all_ids_set
 
     raw_data_list = [
         get_value_from_redis(key=id_data) for id_data in membership_list
@@ -62,6 +61,73 @@ def scenario_use_min_ram_sequence_not_important_v2(
     return clean_data
 
 
-if __name__ == "__main__":
-    # scenario_use_min_ram_sequence_not_important_v1(get_list_keys_with_data())
-    scenario_use_min_ram_sequence_not_important_v2(get_list_keys_with_data())
+@cpu_monitor_decorator(r_client=usage_r_client)
+@ram_monitor_decorator(r_client=usage_r_client)
+def scenario_v3(
+    key_with_data: List[str], **kwargs
+) -> list[TenderDataValidator]:
+    membership_list = []
+
+    for id_ in create_iterator_from_redis_keys():
+        if id_ in key_with_data:
+            membership_list.append(id_)
+
+    clean_data = []
+    for id_data in membership_list:
+        clean_data.append(
+            TenderDataValidator(**get_value_from_redis(key=id_data))
+        )
+
+    return clean_data
+
+
+@cpu_monitor_decorator(r_client=usage_r_client)
+@ram_monitor_decorator(r_client=usage_r_client)
+def scenario_v4(
+    key_with_data: List[str], **kwargs
+) -> list[TenderDataValidator]:
+    all_ids_set = set(create_iterator_from_redis_keys())
+
+    membership_list = set(key_with_data) & all_ids_set
+
+    clean_data = []
+    for id_data in membership_list:
+        clean_data.append(
+            TenderDataValidator(**get_value_from_redis(key=id_data))
+        )
+
+    return clean_data
+
+
+@cpu_monitor_decorator(r_client=usage_r_client)
+@ram_monitor_decorator(r_client=usage_r_client)
+def scenario_v5(key_with_data: List[str], **kwargs) -> List[str]:
+    all_ids_set = set(create_iterator_from_redis_keys())
+
+    membership_list = set(key_with_data) & all_ids_set
+
+    clean_data = []
+    for id_data in membership_list:
+        clean_data.append(
+            TenderDataValidator(
+                **get_value_from_redis(key=id_data)
+            ).model_dump_json()
+        )
+
+    return clean_data
+
+
+@cpu_monitor_decorator(r_client=usage_r_client)
+@ram_monitor_decorator(r_client=usage_r_client)
+def scenario_v6(key_with_data: List[str], **kwargs) -> None:
+    all_ids_set = set(create_iterator_from_redis_keys())
+
+    membership_list = set(key_with_data) & all_ids_set
+
+    for id_data in membership_list:
+        usage_r_client.set(
+            id_data,
+            TenderDataValidator(
+                **get_value_from_redis(key=id_data)
+            ).model_dump_json(),
+        )
