e2d9e80f5bc74d25f7a9dc7746d4e0cc9f327d6f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 06:42:52 +0200

    refactor: Simplify RAM usage monitoring and data handling
    This commit refactors the RAM usage monitoring functionality to:
    
    - Remove unused `usage_r_client` import and `print_ram_usage` function.
    - Improve the logic for appending RAM usage data to `ram_usage_results`.
    - Remove `is_detail` and `to_console` parameters from `ram_monitor_decorator`.
    - Make `r_client` optional in `ram_monitor_decorator`.

--- performance_monitoring/ram.py
+++ performance_monitoring/ram.py
@@ -7,8 +7,6 @@ from typing import Callable, List, Tuple, Optional
 import psutil
 from redis import Redis
 
-from utils.redis_for_save_usage_data import usage_r_client
-
 monitoring = False
 ram_usage = []
 ram_usage_results = {}
@@ -24,24 +22,10 @@ def save_data_to_usage_results(
             r_client.rpush(f"ram_usage_{func_name}", json.dumps(ram_data))
         except Exception as exc:
             raise Exception(f"Failed to save RAM usage data to Redis: {exc}")
-    if func_name not in ram_usage_results:
-        ram_usage_results[func_name] = []
-    ram_usage_results[func_name].append(ram_data)
-
-
-def print_ram_usage(
-    mem_usage: list,
-    interval: float,
-    func_name: str,
-    is_detail: bool,
-) -> None:
-    print(f"📊 Using RAM in the process of performing a '{func_name}':")
-    if is_detail:
-        for i, usage in enumerate(mem_usage):
-            print(f"  Time {i * interval:.2f} seconds: {usage:.2f} MB")
-
-    max_mem = max(mem_usage) if mem_usage else 0
-    print(f"  📊 Peak use RAM: {max_mem:.2f} MB")
+    else:
+        if func_name not in ram_usage_results:
+            ram_usage_results[func_name] = []
+        ram_usage_results[func_name].append(ram_data)
 
 
 def monitor_ram_usage(interval: float = 0.1) -> None:
@@ -56,10 +40,7 @@ def monitor_ram_usage(interval: float = 0.1) -> None:
 
 
 def ram_monitor_decorator(
-    interval: float = 0.1,
-    r_client: Optional[Redis] = usage_r_client,
-    is_detail: Optional[bool] = False,
-    to_console: Optional[bool] = False,
+    r_client: Optional[Redis] = None,
     save_data: Optional[bool] = True,
     is_enabled: Optional[bool] = True,
 ) -> Callable:
@@ -84,13 +65,6 @@ def ram_monitor_decorator(
             finally:
                 monitoring = False
                 thread.join()
-            if to_console:
-                print_ram_usage(
-                    ram_usage,
-                    interval,
-                    kwargs.get("func_name", func.__name__),
-                    is_detail,
-                )
             if save_data:
                 save_data_to_usage_results(
                     func_name=kwargs.get("func_name", func.__name__),
