f66c5785bb923724fa335d0c286ec0822b943065
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 07:26:29 +0200

    refactor: Remove unnecessary decoding in ID generation functions
    The `generate_ids_*` functions were decoding the IDs from bytes to strings.
    This commit refactors the functions to directly accept strings, removing the unnecessary decoding step within the functions. This simplifies the code and assumes the input iterator already yields strings.

--- utils/get_ids.py
+++ utils/get_ids.py
@@ -3,36 +3,36 @@ from typing import List, Dict, Iterator, Set
 
 
 def generate_ids_in_list(
-    redis_iterator: Iterator[bytes],
+    redis_iterator: Iterator[str],
 ) -> List[str]:
     ids_list = []
     for uid in redis_iterator:
-        ids_list.append(uid.decode())
+        ids_list.append(uid)
     return ids_list
 
 
 def generate_ids_in_set(
-    redis_iterator: Iterator[bytes],
+    redis_iterator: Iterator[str],
 ) -> Set[str]:
     ids_set = set()
     for uid in redis_iterator:
-        ids_set.add(uid.decode())
+        ids_set.add(uid)
     return ids_set
 
 
 def generate_ids_in_deque(
-    redis_iterator: Iterator[bytes],
+    redis_iterator: Iterator[str],
 ) -> deque[str]:
     ids_deque = deque([])
     for uid in redis_iterator:
-        ids_deque.append(uid.decode())
+        ids_deque.append(uid)
     return ids_deque
 
 
 def generate_ids_in_dict(
-    redis_iterator: Iterator[bytes],
+    redis_iterator: Iterator[str],
 ) -> Dict[str, None]:
     ids_dict = {}
     for uid in redis_iterator:
-        ids_dict[uid.decode()] = None
+        ids_dict[uid] = None
     return ids_dict
