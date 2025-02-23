f633d90b048cd1fc061f324f3e9cbab7fc99f9af
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-23 06:49:43 +0200

    refactor: Simplify benchmark and add Redis usage tracking
    This commit refactors the benchmarking code to remove redundant functions and directly utilize the `measure_resource_usage` function.
    
    Additionally, it introduces Redis usage tracking to monitor CPU and RAM usage during benchmarking, providing more comprehensive performance insights. The memory report functionality is configurable via the `memory_report` parameter.

--- benchmark/benchmark_create_collections.py
+++ benchmark/benchmark_create_collections.py
@@ -1,51 +1,32 @@
 from collections import deque
-from typing import Union, Callable, Iterator, Optional
+from typing import Union, Callable, Iterator
 
 from performance_monitoring.cpu import cpu_monitor_decorator
 from performance_monitoring.memory import memory_object_report
 from performance_monitoring.ram import ram_monitor_decorator
-from utils.get_ids import generate_ids_in_list
+from utils.get_ids import (
+    generate_ids_in_list,
+    generate_ids_in_deque,
+    generate_ids_in_set,
+    generate_ids_in_dict,
+)
+from utils.redis_for_save_usage_data import usage_r_client
 from utils.redis_test_data_loader import (
     redis_client,
 )
 
 
-@ram_monitor_decorator()
-@cpu_monitor_decorator()
+@cpu_monitor_decorator(r_client=usage_r_client, is_enabled=True)
+@ram_monitor_decorator(r_client=usage_r_client, is_enabled=True)
 def measure_resource_usage(
     redis_iterator: Iterator[bytes],
     func: Callable,
     **kwargs,
 ) -> Union[list, dict, set, deque]:
-    return func(redis_iterator)
-
-
-def benchmark_id_generation(
-    func: Callable,
-    func_name: str,
-    iterator: Iterator[bytes],
-    memory_report: Optional[bool] = False,
-) -> None:
-    ids_collection = measure_resource_usage(
-        func=func,
-        redis_iterator=iterator,
-        func_name=func_name,
-    )
-
-    if memory_report:
+    ids_collection = func(redis_iterator)
+    if kwargs.get("memory_report", True):
         memory_object_report(ids_collection)
-
-
-def main_benchmark_ids_generation(
-    func: Callable, func_name: str, repeat: int = 2
-) -> None:
-    for _ in range(repeat):
-        benchmark_id_generation(
-            func=func,
-            func_name=func_name,
-            iterator=redis_client.scan_iter("*"),
-            memory_report=False,
-        )
+    return ids_collection
 
 
 if __name__ == "__main__":
@@ -56,6 +37,9 @@ if __name__ == "__main__":
         - generate_ids_in_set -> Generate IDs in Set
         - generate_ids_in_dict -> Generate IDs in Dict
     """
-    main_benchmark_ids_generation(
-        func=generate_ids_in_list, func_name="Generate IDs in List"
+    measure_resource_usage(
+        func=generate_ids_in_list,
+        redis_iterator=redis_client.scan_iter("*"),
+        func_name="Generate IDs in List",
+        memory_report=False,
     )
